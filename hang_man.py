251385b62bb53a3acca7ead9960bed253dcdf5b5
Carlos Yanez <cyanez_cly@yahoo.com>
2022-02-26 18:57:43 -0300

    Hangman function details

--- hang_man.py
+++ hang_man.py
@@ -21,15 +21,17 @@ def hangman():
     word_check = set(palabra) #letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
-
-    while len(word_check) > 0:
-       
-        print('Used letters: ',' '.join(used_letters))
+    lifes = 6 # max number of attempts allowed
+    
+    while len(word_check) > 0 and lifes > 0:
+        
+        print(f'YOU HAVE {lifes} TURNS LEFT\nUsed letters: ',' '.join(used_letters))
         print('----------------------------------------------------------')
         
-        guessin_progress = [letters if letters in used_letters else '-' for letters in palabra]
+        guessin_progress = [letters if letters in used_letters else '-' for letters in palabra] # a loop which validate every single single letter in the hidden word and return just the letters that we guessed right.
         print('current progress: ', ' '.join(guessin_progress))
-        user_letter = input('Guess letter ----> ').upper()
+
+        user_letter = input('Guess letter ----> ').upper() #user input guess
         print('\n')
         
         if user_letter in alphabet - used_letters:
@@ -37,14 +39,20 @@ def hangman():
 
             if user_letter in word_check:
                 word_check.remove(user_letter)
+            else:
+                lifes -= 1 # subtract one attempt until there is nothing left.
+                print(f'{user_letter} is not in the word, try hard!')
         
         elif user_letter in used_letters:
-            print(f'You have already used this one: {user_letter}. Try a different one!!!')
+            print(f'\t\t\t\tYou have already used this one: "{user_letter}". Try a different one!!!')
         else:
             print('invalid character')
     
-    guessin_progress = [letters if letters in used_letters else '-' for letters in palabra]
-    print('\n\t\t\tCongratulations!!! You got the word: ', ' '.join(guessin_progress))
+    if lifes != 0:
+        guessin_progress = [letters if letters in used_letters else '-' for letters in palabra]
+        print('\n\t\t\t\tYOU WIN Congratulations!!! You got the word: ', ' '.join(guessin_progress))
+    else:
+        print('\n\t\t\t\tYOU LOSE :( the word was: ', palabra)
         
 play = hangman()
   
